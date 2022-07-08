794bd1d6f4a50c81a733f5b1c4d4532d5d89d926
PavelAlekseev <pavelbooking.com@yandex.ru>
2022-07-08 11:56:06 +0500

    mesto

--- scripts/script.js
+++ scripts/script.js
@@ -19,31 +19,6 @@ const buttonProfileOpen = document.querySelector ('.profile__button');
 const buttonProfileClose = document.querySelector ('.button-close');
 const buttonPhotoAddOpen = document.querySelector ('.profile__button-photo');
 const buttonPhotoAddClose = document.querySelector ('.button-photo-close');
-const initialCards = [
-  { 
-    link: "https://kudann.ru/uploads/29aa4243cab801472fa13993d11909ec.jpeg", title: "Нижний Новгород" 
-  },
-  { 
-    link: "https://www.syl.ru/misc/i/ai/378308/2436551.jpg", 
-    title: "Калининград" 
-  },
-  { 
-    link: "https://s.mediasalt.ru/images/375/375941/original.jpg", 
-    title: "Байкал" 
-  },
-  { 
-    link: "https://1001bus-ufa.ru/images/1001/sankt1.jpg", 
-    title: "Санкт-Петербург" 
-  },
-  { 
-    link: "https://avatars.mds.yandex.net/get-zen_doc/3930378/pub_5f8fdd3a6dc8f92eda7141da_5f8fde39c2b29d22942af341/scale_1200", 
-    title: "Екатеринбург" 
-  },
-  { 
-    link: "https://tripplanet.ru/wp-content/uploads/europe/russia/chelyabinsk/kirovka.jpg", 
-    title: "Челябинск" 
-  }
-]
 
 //Функция открытия попапа
 
@@ -119,11 +94,12 @@ function createCard (element){
 
   //Open photo in popup
   const image = item.querySelector ('.photo__image');
+  const text = item.querySelector ('.photo__text');
 
   image.addEventListener ('click', function(){
     modalImg.src = image.src;
     modalImg.alt = image.alt;
-    captionText.textContent = image.alt;
+    captionText.textContent = text.textContent;
     openPopup (popupImage);
   });
 
